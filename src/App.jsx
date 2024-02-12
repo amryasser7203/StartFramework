8ea1f5c86b8f64a67e353ed2fe2dd66d53d23277
Amr yasser <amry7203@gmail.com>
2024-02-13 01:44:20 +0300

    upload

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home/Home';
+import { RouterProvider, createBrowserRouter, createHashRouter } from 'react-router-dom';
+import MasterLayout from './components/MasterLayout/MasterLayout';
+import About from './components/About/About';
+import Portfolio from './components/Portfolio/Portfolio';
+import Contact from './components/Contact/Contact';
+import NotFound from './components/NotFound/NotFound';
+
+const router = createHashRouter([{
+  path: '', element: <MasterLayout />, children: [
+    { path: '', element: <Home /> },
+    { path: 'home', element: <Home /> },
+    { path: 'about', element: <About /> },
+    { path: 'portfolio', element: <Portfolio /> },
+    { path: 'contact', element: <Contact /> },
+    { path: '*', element: <NotFound /> }
+  ]
+}])
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
